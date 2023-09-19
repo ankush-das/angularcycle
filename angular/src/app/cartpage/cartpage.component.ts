f7e43a1e1f5d71f79523dfe4f273f041d31ad0cc
ankush das <ankush-das>
2023-09-19 14:49:04 +0530

    before swap

---
+++ angular/src/app/cartpage/cartpage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartpage',
+  templateUrl: './cartpage.component.html',
+  styleUrls: ['./cartpage.component.css']
+})
+export class CartpageComponent {
+}
---
+++ angular/src/app/rentpage/rentpage.component.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { AuthService } from '../AuthService';
+
+@Component({
+  selector: 'app-rentpage',
+  templateUrl: './rentpage.component.html',
+  styleUrls: ['./rentpage.component.css']
+})
+export class RentpageComponent {
+
+  newdata: any;
+
+  constructor(private _http: HttpClient, private head: AuthService) { }
+
+  getallcycle() {
+    return this._http.get('http://localhost:8080/api/cycles/list-data');
+  }
+
+  ngOnInit() {
+    this.getallcycle().subscribe({
+      next: (res) => {
+        this.newdata = res;
+        console.log('Success: Response from API:', this.newdata);
+      },
+      error: (error) => {
+        console.error('Error: Failed to fetch data from API:', error);
+      }
+    });
+  }
+
+  borrowCycle(id: number, quantityToBorrow: number) {
+
+    const requestBody = { id, count: quantityToBorrow };
+
+    const url = `http://localhost:8080/api/cycles/${id}/borrow`;
+
+    const headers = this.head.getHeaders();
+
+    this._http.post(url, requestBody, { headers, responseType: 'text' }).subscribe(response => {
+      this.ngOnInit();
+      console.log(`Cycle with ID ${id} rented successfully.`);
+    });
+  }
+
+}
